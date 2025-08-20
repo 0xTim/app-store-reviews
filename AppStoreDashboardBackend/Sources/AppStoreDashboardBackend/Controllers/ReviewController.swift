d2371390b135c4eba9263a8ce07d1c06fd8072c4
Tim <0xtimc@gmail.com>
2025-08-20 02:10:47 +0100

    Add basic get route

--- AppStoreDashboardBackend/Sources/AppStoreDashboardBackend/Controllers/ReviewController.swift
+++ AppStoreDashboardBackend/Sources/AppStoreDashboardBackend/Controllers/ReviewController.swift
@@ -1,11 +1,21 @@
 import Vapor
+import Fluent
 
-struct ReviewController: RouteCollection {
+struct ReviewController: RouteCollection, Sendable {
+    let reviewRepository: any ReviewRepository
+
     func boot(routes: any RoutesBuilder) throws {
-        
+        routes.get("apps", ":appID", "reviews", use: getAllReviews)
     }
 
-    func getAllReviews(req: Request) async throws -> [ReviewDTO] {
-        []
+    @Sendable
+    func getAllReviews(req: Request) async throws -> Page<ReviewDTO> {
+        let appID = try req.parameters.require("appID")
+        let page: Int = req.query["page"] ?? 1
+        let limit: Int = req.query["limit"] ?? 20
+        let result = try await reviewRepository.getAllReviewsPaginated(appId: appID, page: page, limit: limit)
+        let dtoItems = try result.items.map { try $0.toDTO() }
+        let dtoPage = Page(items: dtoItems, metadata: result.metadata)
+        return dtoPage
     }
 }
--- AppStoreDashboardBackend/Sources/AppStoreDashboardBackend/Repositories/ReviewRepository.swift
+++ AppStoreDashboardBackend/Sources/AppStoreDashboardBackend/Repositories/ReviewRepository.swift
@@ -1,6 +1,6 @@
 import FluentKit
 
-protocol ReviewRepository {
+protocol ReviewRepository: Sendable {
     func save(_ review: Review) async throws
     func getAllReviewsPaginated(appId: String, page: Int, limit: Int) async throws -> Page<Review>
 }
